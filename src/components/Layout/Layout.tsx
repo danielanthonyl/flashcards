58717dcde5f26b112f29c2929851b2eac45ec590
danielanthonyl <danskyjs@icloud.com>
2024-03-08 19:09:46 -0800

    feature: add redirect

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,useLocation, useNavigate } from "react-router-dom";
 import classes from "./Layout.module.css";
+import { useEffect } from "react";
 
 export const Layout = () => {
+  const {pathname} = useLocation();
+  const navigate = useNavigate();
+
+
+
+  useEffect(() => {
+    if(pathname === '/'){
+      navigate("/decks");
+    }
+
+  },[pathname, navigate])
+
   return (
     <div className={classes.layout}>
       <Outlet />
